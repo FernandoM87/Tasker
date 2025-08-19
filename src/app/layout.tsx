b6403d970339e216d3962508d9040b3c2cf19140
FernandoM87 <marroquinf656@live.com>
2025-08-19 11:16:23 +0200

    Neons set up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,17 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className=""
-      >
-        {children}
-      </body>
+      <body>
+        <Providers>{children}</Providers> 
+        </body>
     </html>
   );
 }
+
